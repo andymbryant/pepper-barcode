f6439e33a7b068a7c86d72d627af6b77d79f21e7
Andy Bryant <andy@usepepper.com>
2021-04-04 16:46:38 -0400

    working tensorcamera

--- App.js
+++ App.js
@@ -1,12 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Camera } from 'expo-camera';
+import { cameraWithTensors } from '@tensorflow/tfjs-react-native';
+import * as tf from '@tensorflow/tfjs'
+const TensorCamera = cameraWithTensors(Camera);
+
+function handleCameraStream(images, updatePreview, gl) {
+  const loop = async () => {
+    const nextImageTensor = images.next().value
+
+    //
+    // do something with tensor here
+    //
+
+    // if autorender is false you need the following two lines.
+    // updatePreview();
+    // gl.endFrameEXP();
+
+    requestAnimationFrame(loop);
+  }
+  loop();
+}
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+
+  useEffect(() => {
+    (async () => {
+      await tf.ready()
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* <Camera style={styles.camera} type={type}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              setType(
+                type === Camera.Constants.Type.back
+                  ? Camera.Constants.Type.front
+                  : Camera.Constants.Type.back
+              );
+            }}>
+            <Text style={styles.text}> Flip </Text>
+          </TouchableOpacity>
+        </View>
+      </Camera> */}
+      <TensorCamera
+       // Standard Camera props
+       style={styles.camera}
+       type={Camera.Constants.Type.front}
+       // Tensor related props
+      //  cameraTextureHeight={textureDims.height}
+      //  cameraTextureWidth={textureDims.width}
+       resizeHeight={200}
+       resizeWidth={152}
+       resizeDepth={3}
+       onReady={handleCameraStream}
+       autorender={true}
+      />
     </View>
   );
 }
@@ -14,8 +77,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    backgroundColor: 'transparent',
+    flexDirection: 'row',
+  },
+  button: {
+    flex: 0.1,
+    alignSelf: 'flex-end',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 18,
+    color: 'white',
   },
 });
